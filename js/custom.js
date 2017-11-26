c1f513dbd57be6e44892a15ff053c7c7b6a38a8e
Thamodaran <kthamodaran@gmail.com>
2017-11-26 11:01:26 +0530

    Code minify and code change

--- js/custom.js
+++ js/custom.js
@@ -1,37 +1,36 @@
-window.onload = function () {
-    var string = 'mr mahakavi bharathiyar kavithai is a tamilan';
-//    findLongestString(string);
-    var sampleArray = ['Fire', 'Wind', 'Rain'];
-//    arrayJoin(sampleArray);
-    // var replaceString = 'Fire';
-    // stringReplace(replaceString);
-    // currentDate();
-//    returnCurrencies();
-};
-function findLongestString(str) {
+/*
+ * Function for find longest string.
+ */
+function findLongestString() {
+    var str = document.getElementById("find-longest-string").value;
     var sampleStr = str.split(' ');
-    var premap = 0;
     var map1 = sampleStr.map(x=> x.length);
-    var max = Math.max.apply(null, map1);
+    var max = Math.max.apply(null, map1);    //Get max length in array value.
     for (var i = 0; i < map1.length; i++) {
         if (map1[i] == max) {
             maxIndex = i;
         }
     }
-    console.log(sampleStr[maxIndex]);
+    document.getElementById("longest-string").innerHTML = sampleStr[maxIndex];
 }
-
+/*
+ * Function for join array.
+ */
 function arrayJoin() {
     var inputArray = ["Tamil", "English", "Science", "Maths"];
-    var resArray = inputArray.join(",");    
+    var resArray = inputArray.join(",");    //Join the array values.      
     document.getElementById("join-result").innerHTML = resArray;
 }
-
+/*
+ * Function for string replace.
+ */
 function stringReplace() {
     str = document.getElementById("change-string-position").value;
     document.getElementById("changed-string-position").innerHTML = str.charAt(str.length - 1) + '' + str.substring(1, str.length - 1) + '' + str.charAt(0);
 }
-
+/*
+ * Get current date in different formats. 
+ */
 function currentDate() {
     var today = new Date();
     document.getElementById("date-one").innerHTML = today.getMonth() + '-' + today.getDate() + '-' + today.getFullYear();
@@ -39,30 +38,25 @@ function currentDate() {
     document.getElementById("date-three").innerHTML = today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear();
     document.getElementById("date-four").innerHTML = today.getDate() + '/' + today.getMonth() + '/' + today.getFullYear();
 }
-
+/*
+ * Function return amount to coins.
+ */
 function returnCurrencies() {
     var amount = document.getElementById("change-amount-coins").value;
     var coins = [1, 2, 5, 10, 25];
     var tempCoins = [1, 2, 5, 10, 25];
     resultAmt = [];
-    var a = amount;
-    for (var i = 0; i < coins.length; i++) {
-        var b = Math.max.apply(null, tempCoins);
-        if (i >= 1) {
-            var remainder = a % b;
-            var quotient = parseInt(a / b);
-        } else if (i == 0) {
-            var remainder = a % b;
-            var quotient = parseInt(a / b);
+    for (var coinCount = 0; coinCount < coins.length; coinCount++) {
+        var maxCoin = Math.max.apply(null, tempCoins);
+        var remainder = amount % maxCoin;
+        var quotient = parseInt(amount / maxCoin);
+        amount = remainder;
+        for (var maxCoinCount = 0; maxCoinCount < quotient; maxCoinCount++) {
+            resultAmt.push(maxCoin);
         }
-        a = remainder;
-        for (var j = 0; j < quotient; j++) {
-            resultAmt.push(b);
-        }
-        tempCoins.pop(b);
+        tempCoins.pop(maxCoin);
     }
     document.getElementById("changed-amount-coins").innerHTML = resultAmt;
-//    console.log(resultAmt);
 }
 function appendDateResult() {
     html = '<section style="background-color: #FFFFFF; height: 300px;"> <div style="padding: 10px 0 0 0px;" class="output">Question</div><div style="padding: 0 0 0 350px;"> <ol> <li>Get the current date</li><ul> <li>Expected Output :</li><ul> <li>mm-dd-yyyy</li><li>mm/dd/yyyy</li><li>dd-mm-yyyy</li><li>dd/mm/yyyy</li><ul> </ul> </ol> </div></section> <section style="background-color: #FFFFFF; height: 250px;"> <div class="output">Output</div><div style="padding: 70px 0 0 350px;"> Get Current Date : <span onclick="currentDate()">Click Me</span> <span id="date-one">_________</span> <span id="date-two">_________</span> <span id="date-three">_________</span> <span id="date-four">_________</span> </div></section>';
@@ -78,5 +72,9 @@ function appendCoinsResult() {
 }
 function appendjoinResult() {
     html = '<section style="background-color: #FFFFFF; height: 300px;"> <div style="padding: 10px 0 0 0px;" class="output">Question</div><div style="padding: 0 0 0 350px;"> <ol> <li>join all elements of the following array into a string.</li></ol> <ul> <li>Sample array : myColor=["Red", "Green", "White", "Black"];</li><li> Expected Output : "Red,Green,White,Black" "Red,Green,White,Black" "Red+Green+White+Black" </li></ul> </div></section> <section style="background-color: #FFFFFF; height: 250px;"> <div class="output">Output</div><div style="padding: 70px 0 0 350px;"> Sample Array : ["Tamil", "English", "Science", "Maths"] <span onclick="arrayJoin()">Click Me</span> <span id="join-result">_________</span></div></section>';
+    document.getElementById("content").innerHTML = html;
+}
+function appendLongStringResult() {
+    html = '<section style="background-color: #FFFFFF; height: 300px;"> <div style="padding: 10px 0 0 0px;" class="output">Question</div><div style="padding: 0 0 0 250px;"> <ol> <li>Function that accepts a string as a parameter and find the longest word within the string.</li></ol> </div></section> <section style="background-color: #FFFFFF; height: 250px;"> <div class="output">Output</div><div style="padding: 70px 0 0 350px;"> Enter The String : <input type="text" id="find-longest-string" onkeyup="findLongestString()"> <span id="longest-string">_________</span> </div></section>';
     document.getElementById("content").innerHTML = html;
 }
